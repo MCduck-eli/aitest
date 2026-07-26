ce3b610a32853f3f1cf40c27b6c58d911697637f
Eldor Khalikov <eldorabdukhalikov74@gmail.com>
2026-07-26 12:03:57 +0500

    fixed ssr error

--- frontend/app/components/SceneWrapper.tsx
+++ frontend/app/components/SceneWrapper.tsx
@@ -1,24 +1,30 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import dynamic from "next/dynamic";
 import { ParticlesBackground } from "./ParticlesBackground";
 
-export function SceneWrapper() {
-    return (
-        <div className="fixed top-0 left-0 w-[100vw] h-[100vh] pointer-events-none z-0">
-            <Canvas
-                camera={{ position: [0, 0, 10], fov: 60 }}
-                style={{
-                    position: "fixed",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    pointerEvents: "none",
-                }}
-            >
-                <ParticlesBackground />
-            </Canvas>
-        </div>
-    );
-}
+export const SceneWrapper = dynamic(
+    async () => {
+        const { Canvas } = await import("@react-three/fiber");
+        return function WrappedScene() {
+            return (
+                <div className="fixed top-0 left-0 w-screen h-screen pointer-events-none z-0">
+                    <Canvas
+                        camera={{ position: [0, 0, 10], fov: 60 }}
+                        style={{
+                            position: "fixed",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            pointerEvents: "none",
+                        }}
+                    >
+                        <ParticlesBackground />
+                    </Canvas>
+                </div>
+            );
+        };
+    },
+    { ssr: false },
+);
