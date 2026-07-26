9658451318d0e58e91d4def445de4775c72de9bd
Eldor Khalikov <eldorabdukhalikov74@gmail.com>
2026-07-26 12:08:48 +0500

    fix: scene file

--- frontend/app/components/SceneWrapper.tsx
+++ frontend/app/components/SceneWrapper.tsx
@@ -1,33 +1,40 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
 import { ParticlesBackground } from "./ParticlesBackground";
 
 export function SceneWrapper() {
-    const [isClient, setIsClient] = useState(false);
+    const [Component, setComponent] = useState<React.ComponentType | null>(
+        null,
+    );
 
     useEffect(() => {
-        setIsClient(true);
+        const { Canvas } = require("@react-three/fiber");
+
+        function CanvasWrapper() {
+            return (
+                <div className="fixed top-0 left-0 w-screen h-screen pointer-events-none z-0">
+                    <Canvas
+                        camera={{ position: [0, 0, 10], fov: 60 }}
+                        style={{
+                            position: "fixed",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            pointerEvents: "none",
+                        }}
+                    >
+                        <ParticlesBackground />
+                    </Canvas>
+                </div>
+            );
+        }
+
+        setComponent(() => CanvasWrapper);
     }, []);
 
-    if (!isClient) return null;
+    if (!Component) return null;
 
-    return (
-        <div className="fixed top-0 left-0 w-screen h-screen pointer-events-none z-0">
-            <Canvas
-                camera={{ position: [0, 0, 10], fov: 60 }}
-                style={{
-                    position: "fixed",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    pointerEvents: "none",
-                }}
-            >
-                <ParticlesBackground />
-            </Canvas>
-        </div>
-    );
+    return <Component />;
 }
